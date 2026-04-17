a0651bf6725a2cfb25b8885ea1a9ec430557a05e
Lokesh <lokeshsinghbora1102@gmail.com>
2026-04-17 17:29:53 +0530

    removed the backend

--- backend/state.py
+++
@@ -1,7 +0,0 @@
-from typing import TypedDict, Optional, Dict
-
-class InteractionState(TypedDict):
-    input: str                 # user message
-    current_data: Dict         # existing form state
-    tool: Optional[str]
-    output: Optional[Dict]
